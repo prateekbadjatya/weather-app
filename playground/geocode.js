bd205af933a489bc3f8a7ac11edbcb0c36c633f1
prateek <prateek.badjatya@rksv.in>
2018-11-26 15:31:57 +0530

    Chaining Callbacks  Together

--- playground/geocode.js
+++ playground/geocode.js
@@ -10,10 +10,10 @@ request({
     } else if (body.status ===  'ZERO_RESULTS'){
         callback('unable to find address');
     }else if(body.status ===  'OK'){
-        /*  console.log(response);
-            console.log(JSON.stringify(body,undefined,10)) //Pretty Printing Object
-            console.log(`Address:${body.results[0].formatted_address}`);//1301 lombard street philadelphia
-            */
+          /*  console.log(response);
+              console.log(JSON.stringify(body,undefined,10)) //Pretty Printing Object
+              console.log(`Address:${body.results[0].formatted_address}`);//1301 lombard street philadelphia
+          */
            callback(undefined, {
                address: body.results[0].formatted_address,
                lattitude: body.results[0].geometry.location.lat,
@@ -23,3 +23,9 @@ request({
 });
 };
 module.exports.geocodeAddress = geocodeAddress;
+//1194a0c0c47941dc500495d985dec4f1
+//https://api.darksky.net/forecast/[key]/[latitude],[longitude]
+//39.9396284  lat
+//-75.18663959999999  lon
+
+//https://api.darksky.net/forecast/1194a0c0c47941dc500495d985dec4f1/39.9396284,-75.18663959999999
